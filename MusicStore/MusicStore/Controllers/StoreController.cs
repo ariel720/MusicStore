ee2a6eece264a6b5d471c5cf2cbcd1aeaeb879cd
Shinhee Kim <Shinhee.Kim@GeorgianCollege.ca>
2019-09-23 16:49:32 -0400

    add projects

---
+++ MusicStore/MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            return Ok("test");
+        }
+
+        // GET: /Store/Browse?genre=Disco
+        public string Browse(string genre)
+        {
+            string message = HttpUtility.HtmlEncode("Store.Browse, Genre = "
+        + genre);
+
+            return message;
+        }
+
+        // GET: /Store/Details/5
+        public string Details(int id)
+        {
+            string message = "Store.Details, ID = " + id;
+
+            return message;
+        }
+    }
+}
